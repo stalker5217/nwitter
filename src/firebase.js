759d25b23f18ccbf2e898100b12547a61e9f5607
Song <stalker5217@gmail.com>
2020-09-17 14:10:46 +0900

    make firebase key to environment variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCsemTuy2kKFgVqPVMLFEdV1MD2J7z1ANE",
-    authDomain: "nwitter-78b40.firebaseapp.com",
-    databaseURL: "https://nwitter-78b40.firebaseio.com",
-    projectId: "nwitter-78b40",
-    storageBucket: "nwitter-78b40.appspot.com",
-    messagingSenderId: "769345128176",
-    appId: "1:769345128176:web:c9e759a845ff20c493a435"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 export default firebase.initializeApp(firebaseConfig);
