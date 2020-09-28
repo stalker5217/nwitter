213d5e53a94abb43cf53b5ad3c2e991c9ce8fffd
Song <stalker5217@gmail.com>
2020-09-28 22:19:41 +0900

    Set Tweet Section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+	const [nweet, setNweet] = useState("");
+	
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	const onChange = (event) => {
+		const {
+			target: {value},
+		} = event;
+		setNweet(value);
+	}
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input type="text" value={nweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+				<input type="submit" value="Nweet" />
+			</form>
+		</div>
+	);
+}
 export default Home;
