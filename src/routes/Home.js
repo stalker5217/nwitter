a58384fd621fa7e05c16375b7fb7cba341cf1faf
Song <stalker5217@gmail.com>
2020-10-07 23:14:26 +0900

    get nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,25 @@
 import { dbService } from "fbase";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const Home = () => {
 	const [nweet, setNweet] = useState("");
+	const [nweets, setNweets] = useState([]);
+
+	const getNweets = async() => {
+		// Return QuerySnapShot<T>
+		const dbNweets = await dbService.collection("nweets").get();
+		dbNweets.forEach(document => {
+			const nweetObject = {
+				...document.data(),
+				id: document.id,
+			};
+			setNweets((prev) => [nweetObject, ...prev]);
+		});
+	};
+
+	useEffect(() => {
+		getNweets();
+	}, []);
 	
 	const onSubmit = async (event) => {
 		event.preventDefault();
@@ -27,6 +44,17 @@ const Home = () => {
 				<input type="text" value={nweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
 				<input type="submit" value="Nweet" />
 			</form>
+
+			<div>
+				{
+				nweets.map((nweet) => (
+						<div key={nweet.id}>
+							<h4>{nweet.nweet}</h4>
+						</div>
+					)
+				)
+				}
+			</div>
 		</div>
 	);
 }
